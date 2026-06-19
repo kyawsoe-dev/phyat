deb62c8b26dd25fe4e6cc05fbc391f681509a625
Kyaw Soe <kyawsoedeveloper@gmail.com>
2026-06-19 13:03:44 +0630

    consolidate prisma folder: remove apps/api/prisma duplicate, point postinstall to root schema

--- apps/api/prisma/migrations/20260520000000_add_google_auth/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "password_hash" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "google_id" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_google_id_key" ON "users"("google_id");
